27a0c6c81972a44d8890e246eb8139b4551330b6
soundraven <kin9709@gmail.com>
2024-09-10 12:25:43 +0900

    feat: install Element Plus, Sass

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
-    <NuxtLayout>
-        <NuxtPage />
-    </NuxtLayout>
+    <v-app>
+        <div :class="$style.index">
+            <slot />
+        </div>
+    </v-app>
 </template>
 
 <style lang="scss" module>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div :class="$style.index">
+        <UContainer>
+            <div>Hello, World</div>
+        </UContainer>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" module>
+.index {
+    width: 100%;
+    height: 100%;
+
+    box-sizing: border-box; // 전역 CSS로 옮겨야함
+}
+</style>
